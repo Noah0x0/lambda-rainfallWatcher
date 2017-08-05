e99cbad9dca5064920db5a2ff378159a16be62f2
ihara-kenta <mayonnaise.3048.meter@gmail.com>
2017-08-05 19:08:58 +0900

    replace let to const.

--- index.js
+++ index.js
@@ -2,20 +2,20 @@
 
 const request = require('request-promise');
 const iconv = require('iconv-lite');
-const Promise = require('bluebird');
+const promise = require('bluebird');
 const aws = require('aws-sdk');
 const csv = require('csvtojson');
 const s3 = new aws.S3({ apiVersion: '2006-03-01' });
 
 module.exports.handler = (event, context, callback) => {
-    return Promise.coroutine(processEvent)(event, context, callback);
+    return promise.coroutine(processEvent)(event, context, callback);
 }
 
 function *processEvent(event, context, callback) {
-    const REQ_URL = "http://www.data.jma.go.jp/obd/stats/data/mdrr/pre_rct/alltable/pre1h00_rct.csv";
+    const url = "http://www.data.jma.go.jp/obd/stats/data/mdrr/pre_rct/alltable/pre1h00_rct.csv";
 
     const options = {
-        url: REQ_URL,
+        url: url,
         // あとからSHIFT_JISから変換する必要があるため
         encoding: null
     };
@@ -26,11 +26,11 @@ function *processEvent(event, context, callback) {
 
     csv().fromString(data).on('json',(jsonObj)=>{
         // 金沢市のみのデータ取得
-        const KANAZAWA = 56227;
-        if (jsonObj['観測所番号'] == KANAZAWA) {
+        const kanazawa = 56227;
+        if (jsonObj['観測所番号'] == kanazawa) {
             // TODO ファイル名の同意
-            let fileName = 'kanazawa.json';
-            let params = {
+            const fileName = 'kanazawa.json';
+            const params = {
                 Bucket: process.env.S3_BUCKET,
                 Key: fileName,
                 Body: JSON.stringify(jsonObj),
