f66d23cf0538ee3b90721acaa10acebfe8c27c49
ihara-kenta <mayonnaise.3048.meter@gmail.com>
2017-08-05 19:14:06 +0900

    replace to functions.

--- index.js
+++ index.js
@@ -12,37 +12,44 @@ module.exports.handler = (event, context, callback) => {
 }
 
 function *processEvent(event, context, callback) {
-    const url = "http://www.data.jma.go.jp/obd/stats/data/mdrr/pre_rct/alltable/pre1h00_rct.csv";
+    const data = yield requestWaterFall();
 
+    csv().fromString(data).on('json',(jsonObj)=>{
+        // 金沢市のみのデータ取得
+        const kanazawa = 56227;
+        if (jsonObj['観測所番号'] == kanazawa) {
+            putS3(jsonObj);
+        }
+    });
+};
+
+function requestWaterFall() {
+    const url = "http://www.data.jma.go.jp/obd/stats/data/mdrr/pre_rct/alltable/pre1h00_rct.csv";
     const options = {
         url: url,
         // あとからSHIFT_JISから変換する必要があるため
         encoding: null
     };
 
-    const data = yield request(options).then(function (body) {
+    return request(options).then(function (body) {
         return iconv.decode(new Buffer(body, 'binary'), "SHIFT_JIS"); //作成したバッファを使い、iconv-liteでShift-jisからutf8に変換
     });
+}
 
-    csv().fromString(data).on('json',(jsonObj)=>{
-        // 金沢市のみのデータ取得
-        const kanazawa = 56227;
-        if (jsonObj['観測所番号'] == kanazawa) {
-            // TODO ファイル名の同意
-            const fileName = 'kanazawa.json';
-            const params = {
-                Bucket: process.env.S3_BUCKET,
-                Key: fileName,
-                Body: JSON.stringify(jsonObj),
-                ContentType: 'application/json'
-            };
+function putS3(jsonObj) {
+    // TODO ファイル名の同意
+    const fileName = 'kanazawa.json';
+    const params = {
+        Bucket: process.env.S3_BUCKET,
+        Key: fileName,
+        Body: JSON.stringify(jsonObj),
+        ContentType: 'application/json'
+    };
 
-            let putObjectPromise = s3.putObject(params).promise();
-            putObjectPromise.then(function(data) {
-                console.log('Success');
-            }).catch(function(err) {
-                console.log(err);
-            });
-        }
+    let putObjectPromise = s3.putObject(params).promise();
+    putObjectPromise.then(function (data) {
+        console.log('Success');
+    }).catch(function (err) {
+        console.log(err);
     });
-};
+}
