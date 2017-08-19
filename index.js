5b3bfd6c0c6e5ea135088fac49d58cb56d594532
mafuyuk <mafuyuk.m@gmail.com>
2017-08-19 11:14:01 +0900

    change save s3

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const request = require('request-promise');
 const iconv = require('iconv-lite');
 const promise = require('bluebird');
+const moment = require('moment');
 const aws = require('aws-sdk');
 const csv = require('csvtojson');
 const s3 = new aws.S3({ apiVersion: '2006-03-01' });
@@ -47,13 +48,21 @@ function requestRainFall() {
 }
 
 function putS3(jsonObj) {
-    // TODO ファイル名の同意
-    const dt = new Date().getTime();
-    const fileName = `sample_json/kanazawa${dt}.json`;
+    const year = jsonObj['現在時刻(年)'];
+    const month = jsonObj['現在時刻(月)'];
+    const date = jsonObj['現在時刻(日)'];
+    const hours = jsonObj['現在時刻(時)'];
+    const minutes = jsonObj['現在時刻(分)'];
+
+    const mo = moment(`${year}-${month}-${date}T${hours}:${minutes}:00+09:00`).utc();
+
+    const directory = `rainFall/japan/ishikawa/asano/${mo.year()}/${mo.month() + 1}/${mo.date()}/`;
+    const fileName = `${mo.hours()}:${mo.minutes()}:00.json`;
+    console.log(`${directory}${fileName}`);
 
     const params = {
         Bucket: process.env.S3_BUCKET,
-        Key: fileName,
+        Key: `${directory}${fileName}`,
         Body: JSON.stringify(jsonObj),
         ContentType: 'application/json'
     };
